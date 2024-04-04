a0c4c1fdee631e6834451d29e5b8e386573178d7
maiceoull <157168874+maiceoull@users.noreply.github.com>
2024-04-04 03:33:22 -0500

    added support for sprites

--- go_kitty/player.js
+++ go_kitty/player.js
@@ -9,18 +9,46 @@ class Player {
       x: 0,
       y: 0
     };
-    this.width = 100;
-    this.height = 100;
+    this.width = 236;
+    this.height = 138;
     this.scaledX;
     this.scaledY;
     this.scaledWidth;
     this.scaledHeight;
     this.isJumping = false;
+    this.image = document.getElementById('miffyrunleft');
+    this.sprites = {
+      stand: {
+        right: document.getElementById('miffystandright'),
+        left: document.getElementById('miffystandleft'),
+        x: 12,
+        y: 96,
+        width: 236,
+        height: 143
+      },
+      run: {
+        right: document.getElementById('miffyrunright'),
+        left: document.getElementById('miffyrunleft'),
+        x: 12,
+        y: 96,
+        width: 236,
+        height: 138,
+      },
+    }
+    this.currentSprite;
+    this.currentSpriteX;
+    this.currentSpriteY;
+    this.currentSpriteWidth;
+    this.currentSpriteHeight;
+    this.reset = false;
   }
 
   draw() {
-    this.game.ctx.fillStyle = 'black';
+    /*
+    this.game.ctx.fillStyle = 'yellow';
     this.game.ctx.fillRect(this.scaledX, this.scaledY, this.scaledWidth, this.scaledHeight);
+    */
+    this.game.ctx.drawImage(this.currentSprite, this.currentSpriteX, this.currentSpriteY, this.currentSpriteWidth, this.currentSpriteHeight, this.scaledX, this.scaledY, this.scaledWidth, this.scaledHeight);
   }
 
   update() {
@@ -59,8 +87,17 @@ class Player {
   }
 
   resize() {
+    /*
     this.scaledWidth = this.width * this.game.ratio;
     this.scaledHeight = this.height * this.game.ratio;
+    */
+    this.currentSprite = this.sprites.stand.right;
+    this.currentSpriteX = this.sprites.stand.x;
+    this.currentSpriteY = this.sprites.stand.y;
+    this.currentSpriteWidth = this.sprites.stand.width;
+    this.currentSpriteHeight = this.sprites.stand.height;
+    this.scaledWidth = this.currentSpriteWidth * this.game.ratio;
+    this.scaledHeight = this.currentSpriteHeight * this.game.ratio;
     this.scaledX = this.position.x * this.game.ratio;
     this.scaledY = this.position.y * this.game.ratio;
   }
