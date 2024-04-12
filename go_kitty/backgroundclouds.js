a157d4964842cef7dfd7ea7ae6ec743534f2a1f7
maiceoull <157168874+maiceoull@users.noreply.github.com>
2024-04-12 12:52:53 -0500

    added separate platform and background imgs/files

---
+++ go_kitty/backgroundclouds.js
@@ -0,0 +1,44 @@
+class BackgroundClouds {
+  constructor(game) {
+    this.game = game;
+    this.width = 1700;
+    this.height = this.game.baseHeight;
+    this.scaledWidth;
+    this.scaledHeight;
+    this.image = document.getElementById('backgroundclouds');
+    this.position = {
+      x: 0
+    };
+    this.speed = {
+      x: 0
+    };
+  }
+
+  draw() {
+    /*
+    this.game.ctx.fillStyle = 'lightblue';
+    this.game.ctx.fillRect(this.position.x, 0, this.scaledWidth, this.scaledHeight);
+    */
+    this.game.ctx.drawImage(this.image, this.position.x - this.scaledWidth + 1, 0, this.scaledWidth, this.scaledHeight);
+    this.game.ctx.drawImage(this.image, this.position.x, 0, this.scaledWidth, this.scaledHeight);
+    this.game.ctx.drawImage(this.image, this.position.x + this.scaledWidth - 1, 0, this.scaledWidth, this.scaledHeight);
+  }
+
+  resizeXPos() {
+    this.position.x = 0;
+  }
+
+  resize() {
+    this.scaledWidth = this.width * this.game.ratio;
+    this.scaledHeight = this.height * this.game.ratio;
+    this.position.x = 0;
+  }
+
+  moveLeft() {
+    this.position.x -= 1.3 * this.game.ratio;
+  }
+
+  moveRight() {
+    this.position.x += 1.3 * this.game.ratio;
+  }
+}
